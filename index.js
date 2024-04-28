ad335489479b9811228a242c29715bf5baddfd10
naima-tushin <naima39tushin@gmail.com>
2024-04-28 18:59:24 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const cors = require('cors');
+const { MongoClient, ServerApiVersion } = require('mongodb');
+const app = express();
+const port = process.env.PORT || 5000;
+
+//middleware
+app.use(cors());
+app.use(express.json());
+
+// naimatushin21
+// dSMyrROvb1FRIAxG
+const users = [
+    { id: 1, name: 'Sabana', email: "sabana@gmail.com" }
+]
+
+
+
+const uri = "mongodb+srv://naimatushin21:dSMyrROvb1FRIAxG@cluster0.t1oof5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
+
+        const database = client.db("usersDB");
+        const userCollection = database.collection("users");
+
+        app.post('/users', async (req, res) => {
+            const user = req.body;
+            console.log('new user', user);
+            const result = await userCollection.insertOne(user);
+            res.send(result);
+        });
+
+        // // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+app.get('/', (req, res) => {
+    res.send('Users Management Server is Running')
+})
+
+
+
+
+app.listen(port, () => {
+    console.log(`Server is Running on PORT: ${port}`)
+})
