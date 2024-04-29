76843d562a507370b3b690bf31f98d61b1c96fd1
naima-tushin <naima39tushin@gmail.com>
2024-04-30 02:08:18 +0600

    Deployed in vercel

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
@@ -11,9 +12,8 @@ app.use(express.json());
 
 
 
-
-const uri = "mongodb+srv://naimatushin21:dSMyrROvb1FRIAxG@cluster0.t1oof5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.t1oof5v.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+console.log(uri);
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
     serverApi: {
